cee35962671bb8fb18d69b8e09a4001f49666d7e
egashok <djegor2004.com>
2024-11-28 23:08:11 +0600

    feat: layout spa init

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
+import { AiContent } from './components/ai-content'
+import { Header } from './components/header'
+import { QueryInput } from './components/query-input'
 
 function App() {
 
   return (
-    <>
-    
-    </>
+    <div className='root'>
+        
+      <Header/>
+      <AiContent/>
+      <QueryInput/>
+    </div>
   )
 }
 
--- web/src/shared/ui/query-input/query-input.tsx
+++
@@ -1,8 +0,0 @@
-import './query-input.module.scss'
-
-export const QueryInput = () => {
-    return ( 
-        <input className="query-input" type="text" placeholder="Search" />
-     );
-}
- 
