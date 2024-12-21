f87258f2fb1f6d85d82991744841347f7431412e
Manulik <125672053+Otocolobus-manules@users.noreply.github.com>
2024-12-21 15:12:55 +0300

    ref: ref init_container

--- back/src/init_container.py
+++ back/src/init_container.py
@@ -1,17 +1,43 @@
 from dependency_injector import containers, providers
+from pathlib import Path
 
-from back.src.services.message_service import MessageService
+from back.src.utils.VectorStoreService import VectorStoreService
+from back.src.utils.LanguageModelService import LanguageModelService
+from back.src.services.QuestionAnsweringService import QuestionAnsweringService
 
 
 def init_container(settings):
     class Container(containers.DeclarativeContainer):
-        @staticmethod
-        def init_service(service_type):
-            service = providers.Factory(
-                service_type,
-            )
-            return service
+        # Настройки
+        config = providers.Configuration()
 
-        message_service = init_service(service_type=MessageService)
+        # Пути
+        current_dir = Path.cwd()
+        processed_data_path = providers.Object(
+            current_dir.parent / 'data' / 'processed_data' / 'union.parquet'  # Кринж, но хоть работает.
+        )
 
-    return Container()
+        # Сервисы
+        vectorstore_service = providers.Factory(
+            VectorStoreService,
+            persist_path=processed_data_path,
+            embedding_model="llama3.1"
+        )
+
+        language_model_service = providers.Factory(
+            LanguageModelService,
+            model_name="llama3.1",
+            temperature=0
+        )
+
+        # Основной сервис
+        qa_service = providers.Factory(
+            QuestionAnsweringService,
+            vectorstore_service=vectorstore_service,
+            language_model_service=language_model_service
+        )
+
+    container = Container()
+    container.config.from_dict(settings)
+
+    return container
