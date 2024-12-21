7b4fab569790d4e75fbc450ec9063e763ef0db6f
Manulik <125672053+Otocolobus-manules@users.noreply.github.com>
2024-12-21 15:14:10 +0300

    ref: ref user_message_router

--- back/src/routing/user_message_router.py
+++ back/src/routing/user_message_router.py
@@ -2,19 +2,19 @@ from dependency_injector.containers import DeclarativeContainer
 from fastapi import APIRouter, Path
 
 from back.src.schemas.message import Message
-from back.src.services.interfaces.IMessageService import IMessageService
+from back.src.services.interfaces.IModelService import IModelService
 
 
 def message_router(container: DeclarativeContainer) -> APIRouter:
     router = APIRouter(prefix='/user_message', tags=['user_message'])
 
-    service: IMessageService = container.message_service()
+    service: IModelService = container.qa_service()
 
     @router.get("/{message}", response_model=Message)
     async def get_message(
             message: str = Path(..., description="Сообщение от ботика"),
     ) -> Message:
 
-        return await service.get_message(message)
+        return await service.get_answer(message)
 
     return router
